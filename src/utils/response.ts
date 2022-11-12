0dc3de78ec879581e2ae78851a5b0cd07b6d84df
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-11-12 14:58:07 +0700

    add response utils

---
+++ src/utils/response.ts
@@ -0,0 +1,14 @@
+import { Response } from 'express';
+
+const response = (
+  res: Response,
+  statusCode: number,
+  message: string,
+  data: any
+) =>
+  res.status(statusCode).json({
+    code: statusCode,
+    status: statusCode < 400 ? 'Success' : 'Failed',
+    message,
+    data,
+  });
