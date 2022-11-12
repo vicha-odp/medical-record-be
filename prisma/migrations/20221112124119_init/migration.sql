0eff9af5dc68f44965ddcf7fc751930237e76a2b
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-11-12 20:04:42 +0700

    others and new schema

--- prisma/migrations/20221112124119_init/migration.sql
+++ prisma/migrations/20221112124119_init/migration.sql
@@ -1,23 +1,19 @@
 -- CreateEnum
 CREATE TYPE "Categories" AS ENUM ('Psychiatry', 'Neurology');
 
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Patient', 'Doctor', 'Admin');
 
 -- CreateTable
 CREATE TABLE "Admin" (
     "adminId" SERIAL NOT NULL,
-    "govId" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'Admin',
+    "govId" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" INTEGER,
 
     CONSTRAINT "Admin_pkey" PRIMARY KEY ("adminId")
 );
@@ -25,14 +21,14 @@ CREATE TABLE "Admin" (
 -- CreateTable
 CREATE TABLE "Doctor" (
     "doctorId" SERIAL NOT NULL,
-    "govId" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'Doctor',
+    "govId" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "categoryCategoryId" INTEGER,
-    "userId" INTEGER,
+    "categoryName" "Categories" NOT NULL,
 
     CONSTRAINT "Doctor_pkey" PRIMARY KEY ("doctorId")
 );
@@ -40,13 +36,13 @@ CREATE TABLE "Doctor" (
 -- CreateTable
 CREATE TABLE "Patient" (
     "patientId" SERIAL NOT NULL,
-    "govId" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'Patient',
+    "govId" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" INTEGER,
 
     CONSTRAINT "Patient_pkey" PRIMARY KEY ("patientId")
 );
@@ -66,7 +62,8 @@ CREATE TABLE "MedicalRecord" (
 CREATE TABLE "Category" (
     "categoryId" SERIAL NOT NULL,
     "categoryName" "Categories" NOT NULL,
-    "doctorDoctorId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("categoryId")
 );
@@ -96,28 +93,22 @@ CREATE UNIQUE INDEX "Patient_govId_key" ON "Patient"("govId");
 CREATE UNIQUE INDEX "Patient_email_key" ON "Patient"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Category_categoryName_key" ON "Category"("categoryName");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_CategoryToMedicalRecord_AB_unique" ON "_CategoryToMedicalRecord"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_CategoryToMedicalRecord_B_index" ON "_CategoryToMedicalRecord"("B");
 
 -- AddForeignKey
-ALTER TABLE "Admin" ADD CONSTRAINT "Admin_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Doctor" ADD CONSTRAINT "Doctor_categoryCategoryId_fkey" FOREIGN KEY ("categoryCategoryId") REFERENCES "Category"("categoryId") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Doctor" ADD CONSTRAINT "Doctor_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Patient" ADD CONSTRAINT "Patient_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "MedicalRecord" ADD CONSTRAINT "MedicalRecord_patientPatientId_fkey" FOREIGN KEY ("patientPatientId") REFERENCES "Patient"("patientId") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Doctor" ADD CONSTRAINT "Doctor_categoryName_fkey" FOREIGN KEY ("categoryName") REFERENCES "Category"("categoryName") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "MedicalRecord" ADD CONSTRAINT "MedicalRecord_doctorDoctorId_fkey" FOREIGN KEY ("doctorDoctorId") REFERENCES "Doctor"("doctorId") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MedicalRecord" ADD CONSTRAINT "MedicalRecord_patientPatientId_fkey" FOREIGN KEY ("patientPatientId") REFERENCES "Patient"("patientId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_CategoryToMedicalRecord" ADD CONSTRAINT "_CategoryToMedicalRecord_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("categoryId") ON DELETE CASCADE ON UPDATE CASCADE;
