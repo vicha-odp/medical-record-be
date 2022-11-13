bc278c3487171179bbf57ecc858b28a62f3912b2
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-11-13 11:08:55 +0700

    add category service

--- src/services/categoryService/categoryController.ts
+++ src/services/categoryService/categoryController.ts
@@ -18,7 +18,7 @@ const createCategory = async (req: Request, res: Response) => {
     return response(
       res,
       httpCodes.created,
-      'Head Admin has been created',
+      'Default category has been created',
       createCategory
     );
   } catch (error: any) {
@@ -26,6 +26,66 @@ const createCategory = async (req: Request, res: Response) => {
   }
 };
 
+// Get All Category
+const getAllCategory = async (req: Request, res: Response) => {
+  try {
+    const category = await prisma.category.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return response(
+      res,
+      httpCodes.ok,
+      'Get all Category Data Success',
+      category
+    );
+  } catch (error) {
+    return response(
+      res,
+      httpCodes.forbidden,
+      'Get all Category Data Failed',
+      null
+    );
+  }
+};
+
+// Get Category by Id
+const getCategoryById = async (req: Request, res: Response) => {
+  try {
+    let categoryParams;
+
+    if (req.params.category === Categories.Neurology) {
+      categoryParams = Categories.Neurology;
+    } else if (req.params.category === Categories.Psychiatry) {
+      categoryParams = Categories.Psychiatry;
+    } else {
+      return response(
+        res,
+        httpCodes.notFound,
+        'Page that you looking for is not found.',
+        null
+      );
+    }
+    const category = await prisma.category.findUnique({
+      where: {
+        categoryName: categoryParams,
+      },
+      include: {
+        doctor: true,
+        medicalRecord: true,
+      },
+    });
+
+    return response(res, httpCodes.ok, 'Get Category Data Success', category);
+  } catch (error) {
+    return response(res, httpCodes.forbidden, 'Get Category Data Failed', null);
+  }
+};
+
 export default {
   createCategory,
+  getAllCategory,
+  getCategoryById,
 };
--- src/services/categoryService/categoryRoute.ts
+++ src/services/categoryService/categoryRoute.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import createCategory from './categoryController';
+import category from './categoryController';
 
 const router: Router = Router();
 
 // create
-router.post('/category/createDefaultCategory', createCategory.createCategory);
+router.post('/category/createDefaultCategory', category.createCategory);
+// get all
+router.get('/category', category.getAllCategory);
+// get by id
+router.get('/category/:category', category.getCategoryById);
 
 export default router;
