c38a64fc13ef8a82e84fd4c720519f7b3ecfe8a3
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2022-11-20 20:03:05 +0700

    initialize web3 in server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,32 @@
 import express, { Express, Response, Request } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
 
+import { smartcontractAbi } from './config';
 import router from './routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const address = process.env.ADRESS_OR_NODE_ACCOUNT;
+const contractAddress = process.env.ADDRESS_CONTRACT;
+const contractABI = smartcontractAbi;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(router);
+
+export const web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider('http://34.128.75.179/bc'));
+
+export const NameContract = new web3.eth.Contract(
+  contractABI as AbiItem[],
+  contractAddress
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Medical Record App');
@@ -20,4 +34,13 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+
+  web3.eth.net
+    .isListening()
+    .then(() =>
+      console.log(
+        '⚡️[server]: Blockchain is connected at host: http://34.128.75.179/bc'
+      )
+    )
+    .catch((e) => console.log('⚡️[server]: Wow. Something went wrong: ' + e));
 });
