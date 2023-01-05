d3493e3ea15449cafb6aa102184d2b3247fb6bfd
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2023-01-05 11:20:31 +0700

    add web3 configuration setter and getter (experimental)

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-const address = process.env.ADRESS_OR_NODE_ACCOUNT;
-const contractAddress = process.env.ADDRESS_CONTRACT;
+const contractAddress = '0xddfed606bde1a4e1f68790a8d07f312bb3ffbbdb';
+const addressAccount = '0xe54cf29b1fe252fcac158af61a69308e00235902';
 const contractABI = smartcontractAbi;
 
 app.use(express.json());
@@ -24,8 +24,10 @@ app.use(cors());
 app.use(router);
 app.use(cookieParser());
 
-export const web3 = new Web3();
-web3.setProvider(new Web3.providers.HttpProvider('http://34.128.75.179/bc'));
+export const web3 = new Web3(
+  new Web3.providers.HttpProvider('http://34.128.75.179/bc')
+);
+// web3.setProvider(new Web3.providers.HttpProvider('http://34.128.75.179/bc'));
 
 export const contract = new web3.eth.Contract(
   contractABI as AbiItem[],
@@ -39,19 +41,39 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 
-  // web3.eth.net
-  //   .isListening()
-  //   .then(() =>
-  //     console.log(
-  //       '⚡️[server]: Blockchain is connected at host: http://34.128.75.179/bc'
-  //     )
-  //   )
-  //   .catch((e) => console.log('⚡️[server]: Wow. Something went wrong: ' + e));
+  web3.eth.net
+    .isListening()
+    .then(() =>
+      console.log(
+        '⚡️[server]: Blockchain is connected at host: http://34.128.75.179/bc'
+      )
+    )
+    .catch((e) => console.log('⚡️[server]: Wow. Something went wrong: ' + e));
 
-  // console.log(contract);
+  web3.eth.getAccounts().then((res: any) => {
+    web3.eth.defaultAccount = res[0];
+  });
 
-  // contract.methods
-  //   .getPatInfo('3403012604000001')
-  //   .call()
-  //   .then((res: any) => console.log(res));
+  contract.methods
+    .addPatient(
+      '12345678',
+      'Cek cek',
+      'Cek cek cek',
+      'Cek cek cek cek',
+      'Cek cek cek cek cek'
+    )
+    .send({
+      from: addressAccount,
+      gas: 4712388,
+      gasPrice: 100000000000,
+    })
+    .then((res: any) => console.log('⚡️[Setter]:  ', res));
+
+  contract.methods
+    .getPatInfo('12345678')
+    .call({ from: addressAccount, gas: 4712388, gasPrice: 100000000000 })
+    .then((res: any) => console.log('⚡️[Getter]:  ', res))
+    .catch((e: any) =>
+      console.log('⚡️[Getter]: Wow. Something went wrong: ' + e)
+    );
 });
