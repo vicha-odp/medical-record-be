223aacbb5b706ce22fd74604a7b35bd7af7fc54f
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2023-01-20 01:09:54 +0700

    add route bc

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Express, Response, Request } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-const cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
 
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
@@ -14,8 +14,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-const contractAddress = '0xddfed606bde1a4e1f68790a8d07f312bb3ffbbdb';
-const addressAccount = '0xe54cf29b1fe252fcac158af61a69308e00235902';
+export const contractAddress = '0x27a01562b027eb8dde3f334da2a114d8c8bc98d5';
+export const addressAccount = '0xf6be359e980e540c7f3cb67a8bd2085419d65429';
 const contractABI = smartcontractAbi;
 
 app.use(express.json());
@@ -25,9 +25,8 @@ app.use(router);
 app.use(cookieParser());
 
 export const web3 = new Web3(
-  new Web3.providers.HttpProvider('http://34.128.75.179/bc')
+  new Web3.providers.HttpProvider('http://localhost:8545')
 );
-// web3.setProvider(new Web3.providers.HttpProvider('http://34.128.75.179/bc'));
 
 export const contract = new web3.eth.Contract(
   contractABI as AbiItem[],
@@ -36,6 +35,74 @@ export const contract = new web3.eth.Contract(
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Medical Record App');
+});
+
+app.get('/bc/getPatient', async (req: Request, res: Response) => {
+  const { govId } = req.body;
+
+  const data = await contract.methods
+    .getPatInfo(govId)
+    .call({ from: contractAddress });
+
+  res.send(data);
+});
+
+app.post('/bc/addPatient', async (req: Request, res: Response) => {
+  const { govId, name, patientAddress, mothersName, fathersName } = req.body;
+
+  const data = await contract.methods
+    .addPatient(govId, name, patientAddress, mothersName, fathersName)
+    .send({
+      from: addressAccount,
+      gas: 4712388,
+      gasPrice: 100000000000,
+    });
+
+  res.send(data);
+});
+
+app.get('/bc/getDoctor', async (req: Request, res: Response) => {
+  const { govId } = req.body;
+
+  const data = await contract.methods
+    .getDrInfo(govId)
+    .call({ from: contractAddress });
+
+  res.send(data);
+});
+
+app.post('/bc/addDoctor', async (req: Request, res: Response) => {
+  const { govId, name, specialist } = req.body;
+
+  const data = await contract.methods.addDoctor(govId, name, specialist).send({
+    from: addressAccount,
+    gas: 4712388,
+    gasPrice: 100000000000,
+  });
+
+  res.send(data);
+});
+
+app.get('/bc/getMedicalRecord', async (req: Request, res: Response) => {
+  const { govId } = req.body;
+
+  const data = await contract.methods
+    .getRecord(govId)
+    .call({ from: contractAddress });
+
+  res.send(JSON.parse(JSON.stringify(`{"data": ${data}}`)));
+});
+
+app.post('/bc/addMedicalRecord', async (req: Request, res: Response) => {
+  const { govId, medicalRecord } = req.body;
+
+  const data = await contract.methods.addRecord(govId, medicalRecord).send({
+    from: addressAccount,
+    gas: 4712388,
+    gasPrice: 100000000000,
+  });
+
+  res.send(data);
 });
 
 app.listen(port, () => {
@@ -53,27 +120,4 @@ app.listen(port, () => {
   web3.eth.getAccounts().then((res: any) => {
     web3.eth.defaultAccount = res[0];
   });
-
-  contract.methods
-    .addPatient(
-      '12345678',
-      'Cek cek',
-      'Cek cek cek',
-      'Cek cek cek cek',
-      'Cek cek cek cek cek'
-    )
-    .send({
-      from: addressAccount,
-      gas: 4712388,
-      gasPrice: 100000000000,
-    })
-    .then((res: any) => console.log('⚡️[Setter]:  ', res));
-
-  contract.methods
-    .getPatInfo('12345678')
-    .call({ from: addressAccount, gas: 4712388, gasPrice: 100000000000 })
-    .then((res: any) => console.log('⚡️[Getter]:  ', res))
-    .catch((e: any) =>
-      console.log('⚡️[Getter]: Wow. Something went wrong: ' + e)
-    );
 });
