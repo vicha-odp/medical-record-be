c8939e0c702b104b9f0361285c65a08ece6a9c76
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2023-01-20 01:18:42 +0700

    add new migration

--- prisma/migrations/20221225044712_add_refresh_token/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Admin" ADD COLUMN     "refreshToken" TEXT;
-
--- AlterTable
-ALTER TABLE "Doctor" ADD COLUMN     "refreshToken" TEXT;
-
--- AlterTable
-ALTER TABLE "Patient" ADD COLUMN     "refreshToken" TEXT;
