a0e1a4c31d43f56aadb2818f1244495952c8fcfa
Ilyas Adiyasa <ilyasadiyasa4@gmail.com>
2023-01-20 01:10:10 +0700

    remove routes for auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,11 @@ import { Router } from 'express';
 import userService from '@services/userService/userRoute';
 import categoryService from '@services/categoryService/categoryRoute';
 import medicalRecordService from '@services/medicalRecordService/medicalRecordRoute';
-import AuthService from '@services/AuthService/authRoute';
 
 const router: Router = Router();
 
 router.use('/userService', userService);
 router.use('/categoryService', categoryService);
 router.use('/medicalRecordService', medicalRecordService);
-router.use('/auth', AuthService);
 
 export default router;
